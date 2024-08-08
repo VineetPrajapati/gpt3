43c626ff59d0134b904c4d56215cf1a80a0b8592
Vineet <infinite2202@gmail.com>
2024-08-08 14:17:20 +0530

    what gpt-3

--- src/components/features/Feature.jsx
+++ src/components/features/Feature.jsx
@@ -1,8 +1,17 @@
-import "./feature.css"
-const Feature = () => {
+import "./feature.css";
+const Feature = ({ title, text }) => {
   return (
-    <div>Feature</div>
-  )
-}
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
 
-export default Feature
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
